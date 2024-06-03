6454b337e3bf9b0692818c605763b15d4bdaf2d5
HuyVo <voquochuy0610@gmail.com>
2024-06-03 15:11:13 +0700

    Notification api

---
+++ src/notifications/firebase.module.ts
@@ -0,0 +1,31 @@
+import { Module, Global } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'path';
+
+@Global()
+@Module({
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: 'FIREBASE_ADMIN',
+      useFactory: (configService: ConfigService) => {
+        // Resolve the path to the service account JSON file
+        const serviceAccountPath = path.resolve(
+          __dirname,
+          '../../src/config/serviceAccountKey.json',
+        );
+        // Load the service account JSON file
+        const serviceAccount = require(serviceAccountPath);
+        // Initialize the Firebase Admin SDK
+        return admin.initializeApp({
+          credential: admin.credential.cert(serviceAccount),
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: ['FIREBASE_ADMIN'],
+})
+export class FirebaseModule {}
