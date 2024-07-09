0661c0cb098d6ea40821fd62083a05403abd1d8e
NamNguyen2710 <nam271000@gmail.com>
2024-07-09 11:42:21 +0700

    validate class module

--- src/class/class.controller.ts
+++ src/class/class.controller.ts
@@ -8,48 +8,108 @@ import {
   Delete,
   HttpCode,
   Query,
+  Request,
+  ForbiddenException,
+  ParseIntPipe,
 } from '@nestjs/common';
 
 import { ClassService } from './class.service';
-import { CreateClassDto } from './dto/create-class.dto';
-import { UpdateClassDto } from './dto/update-class.dto';
-import { QueryClassesSchema } from 'src/class/dto/query-classes.dto';
+import { ZodValidationPipe } from 'src/utils/zod-validation-pipe';
+
+import { CreateClassDto, CreateClassSchema } from './dto/create-class.dto';
+import { UpdateClassDto, UpdateClassSchema } from './dto/update-class.dto';
+import {
+  QueryClassesDto,
+  QueryClassesSchema,
+} from 'src/class/dto/query-classes.dto';
 import { DefaultClassSchema } from 'src/class/dto/response-class.dto';
+import { UserService } from 'src/users/users.service';
 
 @Controller('class')
 export class ClassController {
-  constructor(private readonly classService: ClassService) {}
+  constructor(
+    private readonly classService: ClassService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post()
-  async create(@Body() createClassDto: CreateClassDto) {
+  async create(
+    @Request() request,
+    @Body(new ZodValidationPipe(CreateClassSchema))
+    createClassDto: CreateClassDto,
+  ) {
+    const permission = await this.userService.validateSchoolAdminPermission(
+      request.user.sub,
+      createClassDto.schoolId,
+    );
+    if (!permission)
+      throw new ForbiddenException(
+        'You do not have permission to create a class in this school',
+      );
+
     return this.classService.create(createClassDto);
   }
 
   @Get()
-  async findAll(@Query() query) {
-    const classQuery = QueryClassesSchema.parse(query);
-    const res = await this.classService.findAll(classQuery);
+  async findAll(
+    @Request() request,
+    @Query(new ZodValidationPipe(QueryClassesSchema)) query: QueryClassesDto,
+  ) {
+    const permission = await this.userService.validateSchoolAdminPermission(
+      request.user.sub,
+      query.schoolId,
+    );
+    if (!permission)
+      throw new ForbiddenException(
+        'You do not have permission to view classes in this school',
+      );
+
+    const res = await this.classService.findAll(query);
 
     const classes = res.data.map((c) => DefaultClassSchema.parse(c));
     return { ...res, data: classes };
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return this.classService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.classService.findOne(id);
   }
 
   @Patch(':id')
   async update(
-    @Param('id') id: string,
-    @Body() updateClassDto: UpdateClassDto,
+    @Request() request,
+    @Param('id', ParseIntPipe) id: number,
+    @Body(new ZodValidationPipe(UpdateClassSchema))
+    updateClassDto: UpdateClassDto,
   ) {
-    return this.classService.update(+id, updateClassDto);
+    const classroom = await this.classService.findOne(id);
+
+    const permission = await this.userService.validateSchoolAdminPermission(
+      request.user.sub,
+      classroom.schoolId,
+    );
+    if (!permission)
+      throw new ForbiddenException(
+        'You do not have permission to update this class',
+      );
+
+    return this.classService.update(id, updateClassDto);
   }
 
   @Delete(':id')
   @HttpCode(204)
-  async remove(@Param('id') id: string) {
-    return this.classService.remove(+id);
+  async remove(@Request() request, @Param('id', ParseIntPipe) id: number) {
+    const classroom = await this.classService.findOne(id);
+
+    const permission = await this.userService.validateSchoolAdminPermission(
+      request.user.sub,
+      classroom.schoolId,
+    );
+    if (!permission)
+      throw new ForbiddenException(
+        'You do not have permission to update this class',
+      );
+
+    return this.classService.remove(id);
   }
 }
