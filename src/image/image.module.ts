f10e09b2a1c63a8c3728c5958fe6bbdc12ecb752
NamNguyen2710 <nam271000@gmail.com>
2024-07-16 21:42:32 +0700

    add s3 images

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
+import { Images } from './entities/image.entity';
 
 @Module({
+  imports: [ConfigModule, TypeOrmModule.forFeature([Images])],
   controllers: [ImageController],
   providers: [ImageService],
 })
