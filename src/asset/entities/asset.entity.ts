3c9a0b920bb78831c104f285395beb32c44b528c
HuyVo <voquochuy0610@gmail.com>
2024-07-26 15:38:18 +0700

    Add medical into asset

--- src/asset/entities/asset.entity.ts
+++ src/asset/entities/asset.entity.ts
@@ -1,10 +1,17 @@
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 import { Posts } from 'src/post/entities/post.entity';
 import { Meals } from 'src/menu/entities/meal.entity';
 import { Loa } from 'src/loa/entities/loa.entity';
 import { Albums } from 'src/album/entities/album.entity';
 import { Students } from 'src/student/entities/student.entity';
+import { Medicals } from 'src/medical/entities/medical.entity';
 
 @Entity()
 export class Assets {
@@ -31,4 +38,7 @@ export class Assets {
 
   @ManyToMany(() => Students, (student) => student.studentCvs)
   students: Students[];
+
+  @ManyToOne(() => Medicals, (medical) => medical.assets)
+  medical: Medicals[];
 }
