de4bb42dbb90e70d6fc44f26fffee5553e273c2d
NamNguyen2710 <nam271000@gmail.com>
2024-08-10 15:51:13 +0700

    remove unneccessary stuff

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
 import { Albums } from './entities/album.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AssetModule } from 'src/asset/asset.module';
-import { SchoolModule } from 'src/school/school.module';
 import { UsersModule } from 'src/users/users.module';
 import { ClassModule } from 'src/class/class.module';
 
@@ -12,7 +13,6 @@ import { ClassModule } from 'src/class/class.module';
   imports: [
     AssetModule,
     UsersModule,
-    SchoolModule,
     ClassModule,
     TypeOrmModule.forFeature([Albums]),
   ],
--- src/asset/asset.module.ts
+++ src/asset/asset.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AssetService } from './asset.service';
 import { AssetController } from './asset.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Assets } from './entities/asset.entity';
-import { SchoolModule } from 'src/school/school.module';
 
 @Module({
-  imports: [SchoolModule, TypeOrmModule.forFeature([Assets])],
+  imports: [TypeOrmModule.forFeature([Assets])],
   controllers: [AssetController],
   providers: [AssetService],
   exports: [AssetService],
