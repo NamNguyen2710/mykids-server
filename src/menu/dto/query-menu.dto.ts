a6018a0ba588c998ff46a8627ac436522b1ccbc0
NamNguyen2710 <nam271000@gmail.com>
2024-08-18 15:35:27 +0700

    hotfix query menu

--- src/menu/dto/query-menu.dto.ts
+++ src/menu/dto/query-menu.dto.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const QueryMenuSchema = z.object({
-  classId: z.number(),
+  classId: z.coerce.number(),
   date: z.coerce.date().optional(),
   startDate: z.coerce.date().optional(),
   endDate: z.coerce.date().optional(),
